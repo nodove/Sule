e47dea68df4fb4758b29802eeca3058bac2a0b12
nodove <38495782+nodove@users.noreply.github.com>
2018-04-18 15:06:01 +0300

    sds

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main()
+
 {
-    cout << "Hello world!" << endl;
+   ifstream fin;
+   fin.open ("duom.txt");
+   int m[10], k=0, t=0;
+   while (fin>>m[k])
+    k++;
+   fin.close();
+   for (int a=0; a,k-1; a++)
+    for (int i=t+1; i<k; i++)
+        if (sk[i]<sk[t])
+        t=i;
+    int ks=sk[0];
+    sk[0]=sk[t];
+    sk[t]=ks;
+
+                    }
     return 0;
 }
